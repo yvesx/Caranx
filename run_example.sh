636e3e22c63b3b22e795b20c12dbee48d04c1050
Yves Xie <yxi389@gmail.com>
2013-04-28 20:41:53 -0500

    example query and output

---
+++ run_example.sh
@@ -0,0 +1,20 @@
+curl -XGET http://localhost:8080/instagram_media/efficient_map/_search -d '{
+  "query": {
+    "bool": {
+      "must": [],
+      "must_not": [],
+      "should": [
+        {
+          "query_string": {
+            "default_field": "text",
+            "query": "starbucks"
+          }
+        }
+      ]
+    }
+  },
+  "from": 0,
+  "size": 500,
+  "sort": [],
+  "facets": {}
+}' > insta_starbuck_5000.json
