f052200d8058c36e4c8718aeacba0f8cbab05a7a
Yves Xie <yxi389@gmail.com>
2013-04-30 00:22:48 -0500

    comments

--- NRDCAdobeSIGGRAPH/nrdc_yves.m
+++ NRDCAdobeSIGGRAPH/nrdc_yves.m
@@ -1,7 +1,7 @@
 %% Read files
 %% function for finding visual frequent patterns
-%%
-%%
+%% naive implement with heuristic searching algorithms
+%% for finding bottom-up frequent itemsets
 %% #######
 function [] = nrdc_yves(my_dir,my_out_dir,counter_thres, diversity_counter_thres,s_idx,e_idx)
 %% my_dir = './tmobile/';
@@ -17,11 +17,12 @@ for i = s_idx:e_idx
     %% randomize search
     A = i+1:length(fileIndex);
     B=A(randperm(length(A)));
-    for j = 1:length(B)
-        disp(strcat(num2str(i),'-',num2str(B(j))));
+    for j = 1:length(B) %% shuffle the search order
+        disp(strcat(num2str(i),'-',num2str(B(j)))); %% note the B(j) not j.
         filename = strcat(my_out_dir,num2str(i),'-',num2str(B(j)),'.jpg');
         srcname = strcat(my_out_dir,num2str(i),'.jpg');
         counter = counter + 1;
+        %% stop if a visual itm is already frequent or has gone through enough items
         if (counter > counter_thres || diversity_counter > diversity_counter_thres)
             break;
         end
@@ -45,11 +46,12 @@ for i = s_idx:e_idx
         else
             % Show aligned reference:
             diversity_counter = diversity_counter + 1;
-            bw_align = im2bw(AlignedRef,0.01);
-            bw_align1 = bwconvhull(bw_align);
+            bw_align = im2bw(AlignedRef,0.01); %% work with binary images
+            bw_align1 = bwconvhull(bw_align);  %% fidn the convex hull
+            %% make the AlighedRef same size as src file. Note the padding direction
             bw_align2 = padarray(bw_align1,size(Src(:,:,1))-size(bw_align1),'post');
             a = repmat(bw_align2,[1,1,3]);
-            masked = Src.*a;
+            masked = Src.*a; %% mask the original image and store it.
             %imshow(masked);
             imwrite(masked,filename);
             imwrite(Src,srcname);
